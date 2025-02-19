99176328f2f46f1c1c4cfdf0e4ec9793bf5e094a
Hassene Benammou <hassene.benammou@gmail.com>
2025-02-19 22:42:51 +0000

    Assistant checkpoint: Update header background to use theme colors
    Assistant generated file changes:
    - components/ParallaxScrollView.tsx: Update header background to use theme color
    
    ---
    
    User prompt:
    
    Make the box you added the same color as the theme either white or black

--- components/ParallaxScrollView.tsx
+++ components/ParallaxScrollView.tsx
@@ -90,7 +90,7 @@ const styles = StyleSheet.create({
     position: 'sticky',
     top: 0,
     zIndex: 1000,
-    backgroundColor: '#fff',
+    backgroundColor: headerBackgroundColor[colorScheme],
     paddingTop: 16,
     paddingHorizontal: 16,
   },
