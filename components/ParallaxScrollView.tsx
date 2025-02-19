69a1d8ba5822daaeaeb15ff5be8e53eb1b9fc85b
Hassene Benammou <hassene.benammou@gmail.com>
2025-02-19 15:16:19 +0000

    Assistant checkpoint: Remove header from ParallaxScrollView
    Assistant generated file changes:
    - components/ParallaxScrollView.tsx: Remove header by setting height to 0
    
    ---
    
    User prompt:
    
    remove the blue box at the top of all the pages

--- components/ParallaxScrollView.tsx
+++ components/ParallaxScrollView.tsx
@@ -11,7 +11,7 @@ import { ThemedView } from '@/components/ThemedView';
 import { useBottomTabOverflow } from '@/components/ui/TabBarBackground';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
-const HEADER_HEIGHT = 250;
+const HEADER_HEIGHT = 0;
 
 type Props = PropsWithChildren<{
   headerImage: ReactElement;
