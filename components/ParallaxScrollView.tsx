f4b6f3fc80ed9f0e641d4e8d31af4b161a3cb300
Hassene Benammou <hassene.benammou@gmail.com>
2025-02-19 22:45:21 +0000

    Assistant checkpoint: Add Colors import to ParallaxScrollView
    Assistant generated file changes:
    - components/ParallaxScrollView.tsx: Add Colors import and fix background color
    
    ---
    
    User prompt:
    
    Colors is not defined

--- components/ParallaxScrollView.tsx
+++ components/ParallaxScrollView.tsx
@@ -1,6 +1,7 @@
 
 import type { PropsWithChildren, ReactElement } from 'react';
 import { StyleSheet } from 'react-native';
+import { Colors } from '@/constants/Colors';
 import Animated, {
   interpolate,
   useAnimatedRef,
