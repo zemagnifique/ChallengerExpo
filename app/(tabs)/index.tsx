78359f4cda073c1923bdfadbd735535389294e5f
Hassene Benammou <hassene.benammou@gmail.com>
2025-02-20 00:23:58 +0000

    Restored to '79715b61be941e6dc72ab032f4e12e6a8839d35f'
    Replit-Restored-To:79715b61be941e6dc72ab032f4e12e6a8839d35f

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -290,7 +290,7 @@ const styles = StyleSheet.create({
     width: 50,
     height: 50,
     borderRadius: 25,
-    backgroundColor: useThemeColor({}, 'background'),
+    backgroundColor: '#E8EAF6',
     justifyContent: 'center',
     alignItems: 'center',
   },
