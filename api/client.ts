6a6150dfa367cb8c36820101a019f67bb03db6f9
Hassene Benammou <hassene.benammou@gmail.com>
2025-02-21 19:57:34 +0000

    fix small bug

--- api/client.ts
+++ api/client.ts
@@ -183,7 +183,7 @@ export const ApiClient = {
           headers: {
             "Content-Type": "application/json",
           },
-          body: JSON.stringify({ user_id: userId }),
+          body: JSON.stringify({ user_id: user_id }),
         },
       );
       if (!response.ok) {
